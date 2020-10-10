7bdb63cd6c060c062601fc15c48918d0553177a1
root <root@repo.lab.example.com>
2020-10-10 18:08:49 +0000

    update files

--- playbooks/createuser.sh
+++
@@ -1,3 +0,0 @@
-ansible -i inventory -u root -k -m user -a "name=devops password=devops" all 
-ansible -i inventory  -u root -k -m authorized_key -a "user=devops state=present key='{{ lookup('file', '/home/devops/.ssh/id_rsa.pub') }} '" all
-ansible -i inventory  -u root -k -m copy -a "content='devops ALL=(ALL)	NOPASSWD: ALL' dest=/etc/sudoers.d/devops" all
